9632e5fff0cf7b5905734e701875392992aadd16
Gabriel <Gabrielabdul1@gmail.com>
2021-08-23 23:59:55 +0000

    searches for and replaces a string in the heap of a running process

--- 0x03-proc_filesystem/read_write_heap.py
+++ 0x03-proc_filesystem/read_write_heap.py
@@ -3,31 +3,48 @@
 
 from sys import argv
 
+
 def printUsageAndExit():
     '''prints usage error and exits'''
     print("Usage: {} PID SEARCH WRITE".format(argv[0]))
     exit(1)
 
+
 def findHeapAddress(pid):
-    '''searces /proc/pid/maps file for address of heap'''
+    '''searces /proc/pid/maps file for addr of heap'''
     maps = '/proc/{}/maps'.format(pid)
     try:
         with open(maps, 'r') as file:
             for line in file:
                 if 'heap' in line:
-                    address = line.split(' ')[0]
-                    print("Address of heap:", address)
-                    return address
+                    addr = line.split(' ')[0]
+                    print("Address of heap:", addr)
+                    return addr
     except Exception as e:
         print(e), exit(1)
 
-def replaceStringInHeap(pid, search, replace, address):
+
+def replaceStringInHeap(pid, search, replace, addr):
     '''seeks for search string in mem file of pid, and replaces it'''
     print("searching for:", search)
-    return
+    mem = '/proc/{}/mem'.format(pid)
+    start, stop = int(addr.split('-')[0], 16), int(addr.split('-')[1], 16)
+    print('Heap found!', 'Searching for string!')
+    try:
+        with open(mem, 'rb+') as file:
+            file.seek(start)
+            heap = file.read(stop - start)
+            string = heap.index(bytes(search, "ASCII"))
+            print('Found:', search)
+            file.seek(start + string)
+            file.write(bytes(replace, "ASCII"))
+            print('Replacing {} with {}'.format(search, replace))
+    except Exception as e:
+        print(e), exit(-1)
+
 
 if __name__ == '__main__':
-    if len (argv) != 4:
+    if len(argv) != 4:
         printUsageAndExit()
-    address = findHeapAddress(int(argv[1]))
-    replaceStringInHeap(int(argv[1]), argv[2], argv[3], address)
+    addr = findHeapAddress(int(argv[1]))
+    replaceStringInHeap(int(argv[1]), argv[2], argv[3], addr)
