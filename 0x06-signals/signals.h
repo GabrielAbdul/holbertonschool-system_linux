9b41d5dceaa8020dc2770434e9aed0a94a2ddf05
Gabriel <Gabrielabdul1@gmail.com>
2021-09-24 15:31:56 +0000

    header file

---
+++ 0x06-signals/signals.h
@@ -0,0 +1,23 @@
+#ifndef SIGNALS_H
+#define SIGNALS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
+/* TASK PROTOTYPES */
+int handle_signal(void);
+void (*current_handler_signal(void))(int);
+int handle_sigaction(void);
+void (*current_handler_sigaction(void))(int);
+int trace_signal_sender(void);
+int pid_exist(pid_t pid);
+void all_in_one(void);
+int sigset_init(sigset_t *set, int *signals);
+int signals_block(int *signals);
+int signals_unblock(int *signals);
+int handle_pending(void (*handler)(int));
+
+/* CUSTOM FUNCTION PROTOTYPES */
+void handle_SIGINT(int signum);
+#endif /* SIGNALS_H */
