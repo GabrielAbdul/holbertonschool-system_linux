5f9f8848f05f5a9a9cea22481d847cbb4cb9b9eb
Gabriel <Gabrielabdul1@gmail.com>
2021-10-01 15:01:31 +0000

    kernel style coding fix

--- 0x06-signals/5-signal_describe.c
+++ 0x06-signals/5-signal_describe.c
@@ -8,12 +8,12 @@
  */
 int main(int ac, char **av)
 {
-		if (ac != 2)
-		{
-				printf("Usage: ./5-signal_describe <signum>\n");
-				exit(EXIT_FAILURE);
-		}
+	if (ac != 2)
+	{
+		printf("Usage: ./5-signal_describe <signum>\n");
+		exit(EXIT_FAILURE);
+	}
 
-		printf("%s: %s\n", av[1], strsignal(atoi(av[1])));
-		return (0);
+	printf("%s: %s\n", av[1], strsignal(atoi(av[1])));
+	return (0);
 }
