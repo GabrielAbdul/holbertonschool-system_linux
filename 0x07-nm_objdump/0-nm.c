f7c63d021a3f950fbd702599cbdcb26e8b388519
Gabriel <Gabrielabdul1@gmail.com>
2021-11-04 14:28:06 +0000

    initial commit

---
+++ 0x07-nm_objdump/0-nm.c
@@ -0,0 +1,24 @@
+#include "nm.h"
+
+/**
+ * main - displays the information contained in the ELF sections' headers
+ * @argc: arg count
+ * @argv: args
+ * @env:  enviornment
+ * Return: 0 on success, else 1
+ */
+int main(int argc, char **argv, char **env)
+{
+	char *args[] = {"/usr/bin/nm", "-p", "", NULL};
+
+	(void)argc;
+
+	args[2] = argv[1];
+
+	if (execve("/usr/bin/nm", args, env) == -1)
+	{
+		perror("execv");
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
+}
