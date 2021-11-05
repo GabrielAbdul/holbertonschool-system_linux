7c2e3747731981ff0e608eefc10fc3109bb0515e
Gabriel <Gabrielabdul1@gmail.com>
2021-11-05 04:49:32 +0000

    function that prints Python strings

---
+++ 0x08_CPython/4-python.c
@@ -0,0 +1,33 @@
+#include <Python.h>
+
+/**
+ * name - description
+ * @var:
+ * @var:
+ * Return: int
+ */
+void print_python_string(PyObject *p)
+{
+	char *type;
+
+	printf("[.] string object info\n");
+
+	if (!PyUnicode_Check(p))
+	{
+		printf("  [ERROR] Invalid String Object\n");
+		return;
+	}
+
+	if (PyUnicode_IS_COMPACT(p))
+		type = "compact unicode object";
+	else if (PyUnicode_IS_COMPACT_ASCII(p))
+		type = "compact ascii";
+	else if (PyUnicode_KIND(p) == PyUnicode_WCHAR_KIND)
+		type = "legacy string, not ready";
+	else if (!PyUnicode_IS_COMPACT(p))
+		type = "legacty string, ready";
+
+	printf("  type: %s\n", type);
+	printf("  length: %ld\n", (((PyASCIIObject *)(p))->length));
+	printf("  value: %ls\n", (PyUnicode_AsWideCharString(p, NULL)));
+}
