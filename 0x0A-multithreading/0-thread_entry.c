3b4c192351d327a498bcb3edb356a652929ae469
Gabriel <Gabrielabdul1@gmail.com>
2021-12-12 16:36:05 +0000

    function that will serve as the entry point to a new thread

--- 0x0A-multithreading/0-thread_entry.c
+++ 0x0A-multithreading/0-thread_entry.c
@@ -1,11 +1,14 @@
 #include "multithreading.h"
 
 /**
- * thread_entry -
- *
- * Return: 
+ * thread_entry - serves as the entry point to a new thread
+ * @arg: address of a string that must be printed and followed by a new line
+ * Return: NULL
  */
 void *thread_entry(void *arg)
 {
+	int *id = arg;
 
+	printf("%d\n", *id);
+	pthread_exit(NULL);
 }
