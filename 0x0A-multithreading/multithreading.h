71af7b77256970ed70a3f9f8bffaf922f980e060
Gabriel <Gabrielabdul1@gmail.com>
2021-12-17 15:09:13 +0000

    kernel style coding fix

--- 0x0A-multithreading/multithreading.h
+++ 0x0A-multithreading/multithreading.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <pthread.h>
 
 /**
  * struct pixel_s - RGB pixel
@@ -15,9 +16,9 @@
  */
 typedef struct pixel_s
 {
-    uint8_t r;
-    uint8_t g;
-    uint8_t b;
+	uint8_t r;
+	uint8_t g;
+	uint8_t b;
 } pixel_t;
 
 /**
@@ -29,9 +30,9 @@ typedef struct pixel_s
  */
 typedef struct img_s
 {
-    size_t w;
-    size_t h;
-    pixel_t *pixels;
+	size_t w;
+	size_t h;
+	pixel_t *pixels;
 } img_t;
 
 /**
@@ -42,8 +43,8 @@ typedef struct img_s
  */
 typedef struct kernel_s
 {
-    size_t size;
-    float **matrix;
+	size_t size;
+	float **matrix;
 } kernel_t;
 
 /**
@@ -59,13 +60,13 @@ typedef struct kernel_s
  */
 typedef struct blur_portion_s
 {
-    img_t const *img;
-    img_t *img_blur;
-    size_t x;
-    size_t y;
-    size_t w;
-    size_t h;
-    kernel_t const *kernel;
+	img_t const *img;
+	img_t *img_blur;
+	size_t x;
+	size_t y;
+	size_t w;
+	size_t h;
+	kernel_t const *kernel;
 } blur_portion_t;
 
 void *thread_entry(void *arg);
@@ -73,9 +74,9 @@ int tprintf(char const *format, ...);
 void blur_portion(blur_portion_t const *portion);
 void blur_image(img_t *img_blur, img_t const *img, kernel_t const *kernel);
 int tprintf(char const *format, ...);
-list_t *prime_factors(char const *s);
-task_t *create_task(task_entry_t entry, void *param);
-void destroy_task(task_t *task);
-void *exec_tasks(list_t const *tasks);
+/*list_t *prime_factors(char const *s);*/
+/*task_t *create_task(task_entry_t entry, void *param);*/
+/*void destroy_task(task_t *task);*/
+/*void *exec_tasks(list_t const *tasks);*/
 
 #endif /* MULTITHREADING_H */
