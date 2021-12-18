6df3d1a56e00f291ab32b24c5fdd434cb0eef0d3
Gabriel <Gabrielabdul1@gmail.com>
2021-12-18 14:52:26 +0000

    uses the printf family to print out a given formatted string

--- 0x0A-multithreading/1-tprintf.c
+++ 0x0A-multithreading/1-tprintf.c
@@ -1,11 +1,11 @@
 #include "multithreading.h"
 
 /**
- * tprintf -
- *
- * Return: 
+ * tprintf - use printf to print formatted string
+ * Return: 0
  */
 int tprintf(char const *format, ...)
 {
-
+	printf("[%lu] %s", pthread_self(), format);
+	return (EXIT_SUCCESS);
 }
