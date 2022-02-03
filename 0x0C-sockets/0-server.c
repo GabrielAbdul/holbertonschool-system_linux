cebb95cab7684eaacec49cdcba6c1a510d967093
Gabriel <Gabrielabdul1@gmail.com>
2022-02-03 16:09:06 +0000

    initial commit -- program that opens an IPv4/TCP socket, and listens to traffic on port 12345

---
+++ 0x0C-sockets/0-server.c
@@ -0,0 +1,51 @@
+#include "sockets.h"
+
+int print_error_and_exit(char *str)
+{
+    printf("%s: %s", str, strerror(errno));
+    return (EXIT_FAILURE);
+}
+
+/**
+ * bind_socket - assigns a name to a socket
+ * @socket: socket to assign address (name)
+ * Return: 0 on success, -1 on failure
+ */
+int bind_socket(int socket)
+{
+    int bin;
+    struct sockaddr_in addr;
+
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port = htons(PORT);
+    bin = bind(socket, (struct sockaddr *)&addr, sizeof(addr));
+
+    return (bin);
+}
+
+/**
+ * main - opens an IPv4/TCP socket, and listens to
+ *        traffic on port 12345 (Any address), waits for signal
+ * Return: 0 on succes, -1 on failure
+ */
+int main(void)
+{
+    int socket_fd;
+
+    socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (socket_fd == -1)
+        print_error_and_exit("socket");
+
+    if (bind_socket(socket_fd) == -1)
+        print_error_and_exit("bind");
+
+    if (listen(socket_fd, 1) == -1)
+        print_error_and_exit("listen");
+
+    printf("Server listening on port %d\n", PORT);
+    /* hangs program indefinitely until a signal is recieved */
+    pause();
+    return (EXIT_SUCCESS);
+}
