6025fc8b06eb9831629330a3cb9d483a4f26a64e
Gabriel <Gabrielabdul1@gmail.com>
2022-02-03 16:08:40 +0000

    header file

---
+++ 0x0C-sockets/sockets.h
@@ -0,0 +1,16 @@
+#ifndef _SOCKETS_H_
+#define _SOCKETS_H_
+
+#define PORT 12345
+
+#include <string.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#endif /* SOCKETS_H */
