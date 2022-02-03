669b689a3108f27460832b4d9cc81e43c1557e3f
Gabriel <Gabrielabdul1@gmail.com>
2022-02-03 16:12:19 +0000

    program that connects to a listening server

---
+++ 0x0C-sockets/2-client.c
@@ -0,0 +1,41 @@
+#include "sockets.h"
+
+/**
+ * name - description
+ * @var:
+ * @var:
+ * Return: int
+ */
+int main(int ac, char **av)
+{
+    int socket_fd;
+    char *host;
+    unsigned short port;
+    struct sockaddr_in addr;
+
+    if (ac != 3)
+    {
+        printf("Usage: %s <host> <port>", av[0]);
+        return (EXIT_FAILURE);
+    }
+    host = strcmp(av[1], "localhost") == 0 ? "127.0.0.1" : "localhost";
+    port = atoi(av[2]);
+
+    socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_fd == -1)
+    {
+        printf("socket_fd: %s", strerror(errno));
+        return (EXIT_FAILURE);
+    }
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = inet_addr(host);
+    addr.sin_port = htons(port);
+    if (connect(socket_fd, (struct sockaddr *)&addr, sizeof(addr)) == -1)
+    {
+        printf("connect: %s\n", strerror(errno));
+        return (EXIT_FAILURE);
+    }
+    printf("Connected to %s:%d\n", host, port);
+    return (EXIT_SUCCESS);
+
+}
