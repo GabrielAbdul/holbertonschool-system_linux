760438400c24280e3bcfdec0d6dc756d32eefd37
Gabriel <Gabrielabdul1@gmail.com>
2022-04-21 21:29:39 +0000

    kernel style coding fix, output correct hostname

--- 0x0C-sockets/2-client.c
+++ 0x0C-sockets/2-client.c
@@ -1,41 +1,40 @@
 #include "sockets.h"
 
 /**
- * name - description
- * @var:
- * @var:
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vectpr
  * Return: int
  */
 int main(int ac, char **av)
 {
-    int socket_fd;
-    char *host;
-    unsigned short port;
-    struct sockaddr_in addr;
+	int socket_fd;
+	char *host;
+	unsigned short port;
+	struct sockaddr_in addr;
 
-    if (ac != 3)
-    {
-        printf("Usage: %s <host> <port>", av[0]);
-        return (EXIT_FAILURE);
-    }
-    host = strcmp(av[1], "localhost") == 0 ? "127.0.0.1" : "localhost";
-    port = atoi(av[2]);
+	if (ac != 3)
+	{
+		printf("Usage: %s <host> <port>", av[0]);
+		return (EXIT_FAILURE);
+	}
+	host = strcmp(av[1], "localhost") ==  ? "127.0.0.1" : "localhost";
+	port = atoi(av[2]);
 
-    socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (socket_fd == -1)
-    {
-        printf("socket_fd: %s", strerror(errno));
-        return (EXIT_FAILURE);
-    }
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = inet_addr(host);
-    addr.sin_port = htons(port);
-    if (connect(socket_fd, (struct sockaddr *)&addr, sizeof(addr)) == -1)
-    {
-        printf("connect: %s\n", strerror(errno));
-        return (EXIT_FAILURE);
-    }
-    printf("Connected to %s:%d\n", host, port);
-    return (EXIT_SUCCESS);
-
+	socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (socket_fd == -1)
+	{
+		printf("socket_fd: %s", strerror(errno));
+		return (EXIT_FAILURE);
+	}
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = inet_addr(host);
+	addr.sin_port = htons(port);
+	if (connect(socket_fd, (struct sockaddr *)&addr, sizeof(addr)) == -1)
+	{
+		printf("connect: %s\n", strerror(errno));
+		return (EXIT_FAILURE);
+	}
+	printf("Connected to %s:%d\n", av[1], port);
+	return (EXIT_SUCCESS);
 }
